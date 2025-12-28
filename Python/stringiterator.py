50685821b9eea459de3852b0a26043a878ed0dff
Ananya Sur <ananya.2006.sur@gmail.com>
2025-12-28 18:28:36 +0530

    string iterator more examples

--- Python/stringiterator.py
+++ Python/stringiterator.py
@@ -3,3 +3,17 @@ sum_digits = 0
 for digit in str(abs(num)):
     sum_digits += int(digit)
 print("Sum of the digits:", sum_digits)
+for char in "Hello, World!":
+    print(char)
+for word in "Hello, World!".split():
+    print(word)
+for sentence in "Hello, World!".split("."):
+    print(sentence)
+for word in "Hello, World!".split(" "):
+    print(word)
+for word in "Hello, World!".split(","):
+    print(word)
+for word in "Hello, World!".split("!"):
+    print(word)
+for word in "Hello, World!".split("?"):
+    print(word)
