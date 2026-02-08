fffcbddefcd3fc6ff2bb1d04a87a3b041e8f6c74
Ananya Sur <ananya.2006.sur@gmail.com>
2026-02-08 14:30:47 +0530

    Additional files and changes 08-02-2026

--- Java/addition.java
+++ Java/addition.java
@@ -1,16 +1,10 @@
-import java.util.Scanner; 
-
 public class addition
 {
 	public static void main(String[] args)
 	{
-		Scanner sc = new Scanner(System.in); 
-		System.out.printf("Enter the first number: "); 
-		int num1 = sc.nextInt();
-		System.out.printf("Enter the second number: "); 
-		int num2 = sc.nextInt(); 
-		int result = num1 + num2; 
-		System.out.printf("Result: " + result); 
-		sc.close(); 
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
+		int result = num1 + num2;
+		System.out.println("Result: " + result);
 	}
-} 
+}
